dc838f815d324f84623b0dcb085995fd30cb5067
Vlad Minaev <minaev@haulmont.com>
2021-06-02 14:25:03 +0400

    Add frontend module

---
+++ frontend/src/app/home/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+const HomePage = () => (
+  <div>
+    <FormattedMessage id="home.welcome" /> frontdemo!
+  </div>
+);
+
+export default HomePage;
