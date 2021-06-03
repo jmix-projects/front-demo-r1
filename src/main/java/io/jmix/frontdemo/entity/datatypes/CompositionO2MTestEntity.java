c091cb633edfaa3ee4141d4ed25fe3e2304c831d
Vlad Minaev <minaev@haulmont.com>
2021-06-03 10:31:34 +0400

    Datatypes model

---
+++ src/main/java/io/jmix/frontdemo/entity/datatypes/CompositionO2MTestEntity.java
@@ -0,0 +1,62 @@
+package io.jmix.frontdemo.entity.datatypes;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.*;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.UUID;
+
+@Table(name = "COMPOSITION_O2M_TEST_ENTITY")
+@JmixEntity
+@Entity(name = "CompositionO2MTestEntity")
+public class CompositionO2MTestEntity {
+    @JmixGeneratedValue
+    @Id
+    @Column(name = "ID", nullable = false)
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "DATATYPES_TEST_ENTITY_ID")
+    protected DatatypesTestEntity datatypesTestEntity;
+
+    @Column(name = "QUANTITY")
+    @PositiveOrZero
+    private Integer quantity;
+
+    @InstanceName
+    @Column(name = "NAME")
+    protected String name;
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public DatatypesTestEntity getDatatypesTestEntity() {
+        return datatypesTestEntity;
+    }
+
+    public void setDatatypesTestEntity(DatatypesTestEntity datatypesTestEntity) {
+        this.datatypesTestEntity = datatypesTestEntity;
+    }
+}
---
+++ src/main/java/io/jmix/frontdemo/entity/datatypes/IntegerIdTestEntity.java
@@ -0,0 +1,123 @@
+package io.jmix.frontdemo.entity.datatypes;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Table(name = "INTEGER_ID_TEST_ENTITY")
+@JmixEntity
+@Entity(name = "IntegerIdTestEntity")
+public class IntegerIdTestEntity {
+
+    @Id
+    @JmixGeneratedValue
+    @Column(name = "ID")
+    protected Integer id;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @InstanceName
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "CREATE_TS")
+    private Date createTs;
+
+    @Column(name = "CREATED_BY", length = 50)
+    private String createdBy;
+
+    @Column(name = "UPDATE_TS")
+    private Date updateTs;
+
+    @Column(name = "UPDATED_BY", length = 50)
+    private String updatedBy;
+
+    @Column(name = "DELETE_TS")
+    private Date deleteTs;
+
+    @Column(name = "DELETED_BY", length = 50)
+    private String deletedBy;
+
+    @Version
+    @Column(name = "VERSION", nullable = false)
+    private Integer version;
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public Boolean isDeleted() {
+        return deleteTs != null;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+    public Date getDeleteTs() {
+        return deleteTs;
+    }
+
+    public void setDeleteTs(Date deleteTs) {
+        this.deleteTs = deleteTs;
+    }
+
+    public String getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(String updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Date getUpdateTs() {
+        return updateTs;
+    }
+
+    public void setUpdateTs(Date updateTs) {
+        this.updateTs = updateTs;
+    }
+
+    public String getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Date getCreateTs() {
+        return createTs;
+    }
+
+    public void setCreateTs(Date createTs) {
+        this.createTs = createTs;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
